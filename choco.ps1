afc7c499ed881706dab7b4b4881bc1c3c8052f82
ryancbutler <ryan@ryancbutler.com>
2019-08-29 12:47:18 -0500

    order

--- choco.ps1
+++ choco.ps1
@@ -5,4 +5,4 @@ choco install -y git -params '"/GitOnlyOnPath"'
 $env:Path += ";$env:ProgramFiles\Git\cmd"
 
 #what to install
-choco install -y googlechrome vscode 7zip notepadplusplus putty fslogix bis-f
+choco install -y googlechrome bis-f vscode 7zip notepadplusplus putty fslogix
